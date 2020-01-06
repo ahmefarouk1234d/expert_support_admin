6e97c38281593eae2fcd383036782361b118d055
Faisal Alkhotaifi <falkhotaifi@gmail.com>
2020-01-06 23:33:11 +0300

    finishing up except bounds

--- lib/BlocResources/app_bloc.dart
+++ lib/BlocResources/app_bloc.dart
@@ -4,13 +4,12 @@ import 'package:async/async.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:expert_support_admin/FirebaseResources/firebase_manager.dart';
 import 'package:expert_support_admin/Models/admin_model.dart';
-import 'package:expert_support_admin/Models/order_model.dart';
 import 'package:rxdart/rxdart.dart';
 
 class AppBloc{
   final _admin = BehaviorSubject<AdminUserInfo>();
   FirebaseManager _firebaseManager = FirebaseManager();
-
+  
   Stream<AdminUserInfo> get admin => _admin.stream;
   Sink<AdminUserInfo> get adminChange => _admin.sink;
 
